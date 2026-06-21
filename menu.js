4dfba7692d7276222d1e22272ed7166164185b9b
ryyn08 <reyhanryyn@gmail.com>
2026-06-21 19:25:51 +0700

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,5 @@
+console.log("=== SERVER PROXY MANAGER ===");
+console.log("[1] Start Monitor (Auto)");
+console.log("[2] Cek Status Sekarang");
+console.log("[3] Exit");
+// Anda bisa gunakan library 'readline' untuk bikin menu interaktif
